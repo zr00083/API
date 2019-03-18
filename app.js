b84ad6884782b5e49765518d6b6c7eedd46c43ed
Kieran Rigby <kieran@kierigby.com>
2019-03-18 23:11:58 +0000

    Changed initial output of API default response

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('<h1>Bounty Hunter API</h1>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
