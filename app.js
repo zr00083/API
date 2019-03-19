fc808cbc8799b8676061bf95d644039937c71f91
Kieran Rigby <kieran@kierigby.com>
2019-03-19 02:07:58 +0000

    Ready for master branch'

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+/////////////////////////////////////
+// Bounty Hunter API
+// 2019
+// COM2027 - Group 15
+/////////////////////////////////////
+
+//set port to either the deployment port or 3000 for development
+const PORT = process.env.PORT || 8080
+
+//Including the express app
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
 
-//Root route
+//Root route with just a basic response to let people know this is the API
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the Bounty Hunter API'
   });
 });
 
-app.listen(port, () => console.log(`Bounty Hunter API listening on port ${port}!`))
+//start express listening on the port and just log it to the console.
+app.listen(PORT, () => console.log(`Bounty Hunter API listening on port ${PORT}!`))
