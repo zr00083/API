25c831ee5e429738f5ae8cebd4a186b097051ef8
Kieran Rigby <kieran@kierigby.com>
2019-03-20 13:01:38 +0000

    Added sequelize configuration

--- app.js
+++ app.js
@@ -8,8 +8,9 @@
 const PORT = process.env.PORT || 8080
 
 //Including the express app
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const Sequelize = require('sequelize');
 
 //Root route with just a basic response to let people know this is the API
 app.get('/', (req, res) => {
@@ -17,6 +18,20 @@ app.get('/', (req, res) => {
     message: 'Welcome to the Bounty Hunter API'
   });
 });
+
+
+app.get('/dbtest', (req, res) => {
+  const sequelize = new Sequelize({dialect: 'sqlite'});
+  sequelize
+  .authenticate()
+  .then(() => {
+    res.status(200).send('Connection has been established successfully.');
+  })
+  .catch(err => {
+    res.status(500).send('Unable to connect to the database:');
+  });
+});
+
 
 //start express listening on the port and just log it to the console.
 app.listen(PORT, () => console.log(`Bounty Hunter API listening on port ${PORT}!`))
