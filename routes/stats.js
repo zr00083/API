237ef23d85ae10aae15a3cf9aa729dbf598100a3
Kieran Rigby <kieran@kierigby.com>
2019-04-02 22:40:37 +0100

    Added stats routes and models

--- routes/stats.js
+++ routes/stats.js
@@ -12,19 +12,72 @@ const checkUserMatch = require('../middleware/check-matching-user');
 ////////////////////////////////////////////////////
 
 router.get('/:id/fugitive', checkAuth, (req, res) => {
-  res.send('here');
+  //get all the stats from the fugitive stats table where uid is the users id
+  db.FugitiveStat.findAll({where: {uid: req.params.id }})
+    .then((stats) => {
+      res.status(200).json({stats:stats}); //send the stats in the response
+    });
 });
 
 router.get('/:id/bountyhunter', checkAuth, (req, res) => {
-  res.send('here');
+  //get all the stats from the fugitive stats table where uid is the users id
+  db.BountyHunterStat.findAll({where: {uid: req.params.id }})
+    .then((stats) => {
+      res.status(200).json({stats:stats}); //send the stats in the response
+    });
 });
 
-router.post('/:id/fugitive', checkAuth, (req, res) => {
-  res.send('here');
+router.post('/:id/fugitive', checkAuth, checkUserMatch, (req, res) => {
+  db.User.findAll({where: {id: req.params.id}})
+    .then((users) => {
+      if(users.length > 0){ //if the user exists
+        const stat = {
+          gid: req.body.gid,
+          uid: users[0].id,
+          points: req.body.points,
+          challengesComplete: req.body.challengesComplete,
+          challengesFailed: req.body.challengesFailed,
+          won: req.body.won
+        }
+        //try and create the stat
+        db.FugitiveStat.create(stat)
+          .then((stat) => {
+            //send a success response
+            res.status(200).json({message:"Stat created"});
+          })
+          .catch(() => {
+            res.status(500).json({message:"Unable to create stat"});
+          });
+      }else{ //if the user doesn't exist
+        res.status(404).json({error:"User not found!"}); //send a 404 response
+      }
+    })
 });
 
-router.post('/:id/fugitive', checkAuth, (req, res) => {
-  res.send('here');
+router.post('/:id/bountyhunter', checkAuth, (req, res) => {
+  db.User.findAll({where: {id: req.params.id}})
+    .then((users) => {
+      if(users.length > 0){ //if the user exists
+        const stat = {
+          gid: req.body.gid,
+          uid: users[0].id,
+          points: req.body.points,
+          captures: req.body.captures,
+          won: req.body.won
+        }
+        //try and create the stat
+        db.BountyHunterStat.create(stat)
+          .then((createdStat) => {
+            //send a success response
+            res.status(200).json({message:"Stat created"});
+          })
+          .catch(() => {
+            res.status(500).json({message:"Unable to create stat"});
+          });
+      }else{ //if the user doesn't exist
+        res.status(404).json({error:"User not found!"}); //send a 404 response
+      }
+    })
 });
 
 
