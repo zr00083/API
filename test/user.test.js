651bc91244286162a4e4a2023e36a41cfe64fd73
Lewis <lp00507@surrey.ac.uk>
2019-04-08 21:15:16 +0100

    commit before merge

--- test/user.test.js
+++ test/user.test.js
@@ -19,5 +19,35 @@ describe("User", () => {
           done();
         });
     });
+    it("should register a new user", (done) => {
+      let user = new User ({firstName: "John", lastName: "Smith", username: "johnsmith1", email: "johnsmith@example.com", password:"john123"})
+      user.save((err, user) => {
+        chai.request(app)
+        .get('/')
+        .send({firstName: "John", lastName: "Smith", username: "johnsmith1", email: "johnsmith@example.com", password:"john123"})
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          done();
+      });
+    });
   });
 });
+
+
+// it("should NOT register a user", (done) => {
+//   let user = {
+//     firstName: "John",
+//     lastName: "Smith",
+//     email: "johnsmith@example.com",
+//     password:"john123"
+//   }
+//   chai.request(app)
+//   .get('/')
+//   .end((err, res) => {
+//     res.should.have.status(500);
+//     res.body.should.be.a('object');
+//     res.body.should.have.property('errors');
+//     res.body.errors.should.have.property('username');
+//   })
+// })
